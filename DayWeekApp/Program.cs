dd529115502977f7bee1f2695d97f07e9d57e249
Kasia Rogalska <kgrono@gmail.com>
2022-08-06 11:42:04 +0200

    Aplikacja do zwracania dnia tygodnia z daty urodzin

---
+++ DayWeekApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using DayWeekApp.Core;
+using System;
+
+namespace DayWeekApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var guesser = new DayGuesser();
+            guesser.IntroduceTheApplication();
+            guesser.AskUser();
+            guesser.CalculateTheDate();
+            guesser.PrintDayOfTheWeek();
+        }
+    }
+}
